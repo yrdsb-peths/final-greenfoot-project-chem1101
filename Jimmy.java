59f5b2529293a95648b5572b30c087ea5d50370e
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-18 22:24:18 -0500

    added yipee sound effect
    uh

--- Jimmy.java
+++ Jimmy.java
@@ -12,6 +12,7 @@ public class Jimmy extends Actor
      * Act - do whatever the Jimmy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
