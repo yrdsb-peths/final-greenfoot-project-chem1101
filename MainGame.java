3015cf27de90209b032977b10c943620c1930042
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-18 22:58:46 -0500

    Updated ratio of Main Game Page
    improved aspect ratio

--- MainGame.java
+++ MainGame.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MainGame here.
+ * Main Game Page: Place to actually play the game
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Chem
+ * @version January 2024
  */
 public class MainGame extends World
 {
@@ -16,6 +16,6 @@ public class MainGame extends World
     public MainGame()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(640, 360, 1); 
     }
 }
