96a5dd0b150a3ab4373c483ab4c02a0a3aea0d44
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-19 07:36:42 -0500

    Idk how to animate Jimmy >:(
    gonna try on main branch

--- Jimmy.java
+++ Jimmy.java
@@ -10,6 +10,8 @@ public class Jimmy extends Actor
 {
     GreenfootSound jimmyCaught = new GreenfootSound("equip.mp3");
     //GreenfootImage moving = new GreenfootImage("images/elephant_idle/idle0.png");
+    
+    //animation issues (cant load images)
     GreenfootImage[] moving = new GreenfootImage[10];
     
     /**
@@ -18,11 +20,21 @@ public class Jimmy extends Actor
      */
     public Jimmy()
     {
-        for(int i = 0; i < moving.length; i++)
+        for(int i = 0;i<moving.length;i++)
         {
             moving[i] = new GreenfootImage("images/elephant_idle/idle0.png");
         }
         setImage(moving[0]);
+    }
+    /**
+     * Animating Jimmy Running
+     */
+    int imageIndex = 0;
+    public void animateJimmy()
+    {
+        setImage(moving[imageIndex]);
+        imageIndex = imageIndex + 1 % moving.length;
+        
     }
     
     public void act()
@@ -39,6 +51,8 @@ public class Jimmy extends Actor
         
         //remove furniture if jimmy catches it
         caughtFurn();
+        
+        //animating Jimmy
     }
     
     public void caughtFurn()
