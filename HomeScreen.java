4621e8ac8d636c6fcb65b34a408772f1a84a86e8
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-19 15:14:55 -0500

    Added music to game

--- HomeScreen.java
+++ HomeScreen.java
@@ -8,9 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class HomeScreen extends World
 {
+    GreenfootSound mainMusic = new GreenfootSound("mainGameMusic.mp3");
     //Change into title Image ex:
     //GreenfootImage gameTitle = new GreenfootImage("images/jimmy_idle.PNG");
-    Label titleLabel = new Label("Moving Day w Jimmy",50);
+    //Label titleLabel = new Label("Moving Day w Jimmy",50);
     /**
      * Constructor for objects of class HomeScreen.
      * 
@@ -20,8 +21,22 @@ public class HomeScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(640, 360, 1);
         
-        addObject(titleLabel,getWidth()/2,getHeight()/2);
-        //addObject(gameTitle,getWidth()/2,(getHeight()/2)+50)
+        //Game title
+        GameTitle title = new GameTitle();
+        addObject(title,320,180);
+        
+        //Start Button
+        //StartButton start = new StartButton();
+        Button start = new Button("images/start.PNG",new GameScreen());
+        addObject(start,180,300);
+        
+        //Rules Button
+        //RulesButton rules = new RulesButton();
+        Button rules = new Button("images/rules.PNG",new RuleScreen());
+        addObject(rules,450,300);
+        
+        //addObject(titleLabel,getWidth()/2,getHeight()/2);
+        //addObject(gameTitle,getWidth()/2,(getHeight()/2)+50);
     }
     
     /**
@@ -29,11 +44,14 @@ public class HomeScreen extends World
      */
     public void act()
     {
+        mainMusic.play();
         //change to where they click button to start game
-        if(Greenfoot.isKeyDown("space"))
+        //Not needed anymore
+        /*if(Greenfoot.isKeyDown("space"))
         {
             GameScreen game = new GameScreen();
             Greenfoot.setWorld(game);
         }
+        */
     }
 }
