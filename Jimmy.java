d786b67a00b101a5cd12a70230cb1bca4b6258e6
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-18 22:56:39 -0500

    Restarted!
    Re-Created Main Game Page
    Jimmy Actor

--- Jimmy.java
+++ Jimmy.java
@@ -1,58 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Jimmy here.
+ * Jimmy (Guy who's moving) main character/player of game
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Chinechem Ugochukwu
+ * @version January 2024
  */
 public class Jimmy extends Actor
 {
-    GreenfootSound yipee = new GreenfootSound("equip.mp3");
     /**
      * Act - do whatever the Jimmy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     public void act()
     {
-        if(Greenfoot.isKeyDown("left"))
-        {
-            move(-4);
-        }
-        else if(Greenfoot.isKeyDown("right"))
-        {
-            move(4);
-        }
-        
-        //Removing Furniture once it's touched
-        takeFurn();
-        
-        //Removing Speed boost once it's touched
-        takeSpeed();
-    }
-    
-    //removes and spawns new furniture once caught by jimmy
-    public void takeFurn()
-    {
-        if(isTouching(Furniture.class))
-        {
-            removeTouching(Furniture.class);
-            TitleScreen game = (TitleScreen) getWorld();
-            game.createFurn();
-            game.increaseScore();
-            yipee.play();
-        }
-    }
-    
-    public void takeSpeed()
-    {
-        if(isTouching(Speed.class))
-        {
-            removeTouching(Speed.class);
-            TitleScreen game = (TitleScreen) getWorld();
-            game.createSpeed();
-            game.increaseScore();
-        }
+        // Add your action code here.
     }
 }
--- MainGame.java
+++ MainGame.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class MainGame here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class MainGame extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class MainGame.
      * 
      */
-    public MyWorld()
+    public MainGame()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1); 
     }
 }
