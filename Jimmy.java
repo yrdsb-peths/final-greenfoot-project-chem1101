7a2ae70f878b76a96a041a74365290ccc6479d82
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-19 10:14:03 -0500

    Jimmy is running fast
    Jimmy running in place, change to make him stand later

--- Jimmy.java
+++ Jimmy.java
@@ -9,6 +9,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Jimmy extends Actor
 {
     GreenfootSound jimmyTake = new GreenfootSound("equip.mp3");
+    //GreenfootImage running = new GreenfootImage("images/jimmy_run/idle0.PNG");
+    GreenfootImage[] running = new GreenfootImage[10];
+    /**
+     * Constructor - The code that gets run one time when object is created
+     */
+    public Jimmy()
+    {
+        //part of animating jimmy running (when jimmy is created he's already running)
+        for(int i = 0; i<running.length; i++)
+        {
+            running[i] = new GreenfootImage("images/jimmy_run/idle"+ i+ ".PNG");
+            //makes jimmy a bigger person
+            running[i].scale(120,90);
+        }
+        setImage(running[0]);
+    }
+    
+    //part of animating jimmy running
+    int imageIndex = 0;
+    public void animateRunJimmy()
+    {
+        setImage(running[imageIndex]);
+        //prevents from index getting too big that it surpasses the actual index
+        imageIndex = (imageIndex + 1) % running.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
@@ -21,6 +47,9 @@ public class Jimmy extends Actor
         }
         //Checking if Jimmy has caught furniture, remove furniture because jimmy caught it.
         take();
+        
+        //ACTUALLY Animate Jimmy to make him RUn
+        animateRunJimmy();
     }
     
     //Creates New Furniture and takes away old one if conditions are met (where to add changes
