339662f94ebf1b1ff5a27d74824b147b12e9de1c
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-19 17:17:42 -0500

    randomized furniture

--- Furniture.java
+++ Furniture.java
@@ -9,13 +9,41 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Furniture extends Actor
 {
     int speed = 1;
+    GreenfootImage[] newFurn = new GreenfootImage[6];
     
     /**
      * Act - do whatever the Furniture wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Furniture()
+    {
+        for(int i = 0; i<newFurn.length; i++)
+        {
+            newFurn[i] = new GreenfootImage("images/furniture/furn"+ i+ ".PNG");
+            newFurn[i].scale(60,60);
+            //makes jimmy a bigger person
+        }
+        //animationTimer.mark();
+        
+        //idle.scale(95,105);
+        int num = Greenfoot.getRandomNumber(6);
+        setImage(newFurn[num]);
+    }
+    
+    
     public void act()
     {
+        /*for(int i = 0; i<newFurn.length; i++)
+        {
+            newFurn[i] = new GreenfootImage("images\furniture\furn" + i +".PNG");
+            //makes jimmy a bigger person
+            newFurn[i].scale(95,105);
+        }
+        int num = Greenfoot.getRandomNumber(6);
+        
+        setImage(newFurn[num]);
+        */
+        
         int x = getX();
         int y = getY() + speed;
         setLocation(x,y);
