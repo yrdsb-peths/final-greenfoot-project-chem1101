4d18c41b3bedeb3fd205d05a0eb7e88c7175564b
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-18 22:36:41 -0500

    messing with sound
    when jimmy picks up something it makes an equip sound

--- Jimmy.java
+++ Jimmy.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Jimmy extends Actor
 {
+    GreenfootSound yipee = new GreenfootSound("equip.mp3");
     /**
      * Act - do whatever the Jimmy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,11 +18,11 @@ public class Jimmy extends Actor
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-2);
+            move(-4);
         }
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(2);
+            move(4);
         }
         
         //Removing Furniture once it's touched
@@ -40,6 +41,7 @@ public class Jimmy extends Actor
             TitleScreen game = (TitleScreen) getWorld();
             game.createFurn();
             game.increaseScore();
+            yipee.play();
         }
     }
     
