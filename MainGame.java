26c51d4ce6a2060750f56ff8989758a3bc9cf17f
chem1101 <350745600@gapps.yrdsb.ca>
2024-01-18 23:04:17 -0500

    Added Jimmy and Furniture to Main Game Page
    Added Jimmy to Main Game Page
    Added Furniture 1 to Main Game Page

--- MainGame.java
+++ MainGame.java
@@ -17,5 +17,13 @@ public class MainGame extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(640, 360, 1); 
+        
+        //Adding Jimmy to the World
+        Jimmy jimmy = new Jimmy();
+        addObject(jimmy,50,300);
+        
+        //Adding furniture
+        Furniture furn1 = new Furniture();
+        addObject(furn1, 50, 0);
     }
 }
